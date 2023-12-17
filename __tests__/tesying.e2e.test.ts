7dd6aff15988cb8a6bc6e8a6f89758a16203c851
Marcos Peres <peres_m@mail.ru>
2023-12-17 14:29:35 +0300

    master add home rote

---
+++ __tests__/tesying.e2e.test.ts
@@ -0,0 +1,28 @@
+import { app } from '../src';
+import request = require('supertest');
+import { HTTP_STATUSES } from '../src/models/common';
+import { headersTestConfig } from './config';
+
+const testingPath = '/testing';
+describe('testing api tests', () => {
+  // beforeAll(async () => {
+  //   await request(app).delete('/blogs/all-blogs').set(headersTestConfig).expect(HTTP_STATUSES.NO_CONTENT_204);
+  //   expect(db.blogs).toBeInstanceOf(Array);
+  //   expect(db.blogs).toHaveLength(0);
+  // });
+
+  it('should delete all data', async () => {
+    await request(app).delete(`${testingPath}/all-data`).set(headersTestConfig).expect(HTTP_STATUSES.NO_CONTENT_204);
+    const postsResponse = await request(app).get('/posts').expect(HTTP_STATUSES.OK_200);
+    const blogsResponse = await request(app).get('/blogs').expect(HTTP_STATUSES.OK_200);
+
+    const postsList = postsResponse.body;
+    const blogsList = blogsResponse.body;
+
+    expect(postsList).toBeInstanceOf(Array);
+    expect(postsList).toHaveLength(0);
+
+    expect(blogsList).toBeInstanceOf(Array);
+    expect(blogsList).toHaveLength(0);
+  });
+});
