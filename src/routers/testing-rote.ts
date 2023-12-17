d062f3abd9338c017329fbfe79ee7da343556d8c
mp25 <90695232+marc-peres@users.noreply.github.com>
2023-12-17 14:58:46 +0300

    master remove authValidation from testing/all-data (#4)
    * master remove idValidation from get methods
    
    * master remove authValidation from testing/all-data

--- src/routers/testing-rote.ts
+++ src/routers/testing-rote.ts
@@ -1,11 +1,10 @@
 import { Request, Response, Router } from 'express';
-import { authValidation } from '../middlewares/auth/auth-validation';
 import { HTTP_STATUSES } from '../models/common';
 import { TestingRepository } from '../repositories/testing-repository';
 
 export const testingRoute = Router({});
 
-testingRoute.delete('/all-data', authValidation, (req: Request, res: Response) => {
+testingRoute.delete('/all-data', (req: Request, res: Response) => {
   const result = TestingRepository.deleteAllVideos();
   result ? res.sendStatus(HTTP_STATUSES.NO_CONTENT_204) : res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
 });
