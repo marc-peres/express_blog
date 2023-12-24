4f0e911783400e5bfabab3902839f1e08f923d0d
mp25 <90695232+marc-peres@users.noreply.github.com>
2023-12-24 18:08:30 +0300

    homeWork3 .env (#13)
    * master remove idValidation from get methods
    
    * master remove authValidation from testing/all-data
    
    * master fix validators
    
    * master fix blogId validator
    
    * homeWork2 fix tests, add setting.ts
    
    * homeWork2 fix repository's params
    
    * homeWork3 add mongoDB to project
    
    * homeWork3 change MONGO_URI
    
    * homeWork3 fix postBlogIdValidation
    
    * homeWork3 fix createDate format
    
    * homeWork3 .env

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,12 +3,18 @@ import dotenv from 'dotenv';
 import { MongoClient } from 'mongodb';
 
 dotenv.config();
-const port = process.env.PORT || 3000;
+if (!process.env.PORT) {
+  throw new Error(` ! process.env.PORT doesn't found`);
+}
+if (!process.env.MONGO_URI) {
+  throw new Error(` ! process.env.MONGO_URI doesn't found`);
+}
+if (!process.env.MONGO_LOCAL_URI) {
+  throw new Error(` ! process.env.MONGO_LOCAL_URI doesn't found`);
+}
+const port = process.env.PORT;
 
-const uri =
-  // process.env.MONGO_LOCAL_URI ||
-  // 'mongodb://localhost:27017' ||
-  process.env.MONGO_URI || 'mongodb+srv://peresm:euLCflFNzxuOvhJX@incubator3week.4xlby5h.mongodb.net/?retryWrites=true&w=majority';
+const uri = process.env.MONGO_LOCAL_URI || process.env.MONGO_URI;
 
 const client = new MongoClient(uri);
 export const dataBase = client.db('blogs-hws');
