ea866425adc7c6a5a40a00c3d81132f1deb803eb
Marcos Peres <peres_m@mail.ru>
2023-12-24 18:08:06 +0300

    homeWork3 .env

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,10 @@ import { app } from './setting';
 import { runDb } from './db/db';
 
 dotenv.config();
-const port = process.env.PORT || 3000;
+if (!process.env.PORT) {
+  throw new Error(` ! process.env.PORT doesn't found`);
+}
+const port = process.env.PORT;
 
 app.listen(port, async () => {
   await runDb();
