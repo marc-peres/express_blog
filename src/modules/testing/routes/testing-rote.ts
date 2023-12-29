199d5d1963e1517b1459abec11c7b9b67e3fb365
Marcos Peres <peres_m@mail.ru>
2023-12-29 23:36:19 +0300

    homeWork4 change testingDelete all-data method

--- src/modules/testing/routes/testing-rote.ts
+++ src/modules/testing/routes/testing-rote.ts
@@ -1,14 +1,14 @@
 import { Request, Response, Router } from 'express';
 import { HTTP_STATUSES } from '../../../common/models';
-import { blogsCollection, postsCollection } from '../../../db/db';
+import { dataBase } from '../../../db/db';
 
 export const testingRoute = Router({});
 
 testingRoute.delete('/all-data', async (req: Request, res: Response) => {
   // необходимо иметь права администратора
-  // await dataBase.dropDatabase();
-  await blogsCollection.deleteMany({});
-  await postsCollection.deleteMany({});
+  await dataBase.dropDatabase();
+  // await blogsCollection.deleteMany({});
+  // await postsCollection.deleteMany({});
 
   res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
 });
